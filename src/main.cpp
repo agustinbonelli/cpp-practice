e32f91a07bfa7155758a670d0b55e5f28e85cbfc
agustinbonelli <agustinbonelli@hotmail.com>
2023-10-14 22:44:20 +0000

    Correct conan version and create some basic rxcpp tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,64 +2,52 @@
 #include <nlohmann/json.hpp>
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
-
-#include <NamedType/named_type.hpp>
+#include <rxcpp/rx.hpp>
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
-using namespace fluent;
 
-using FirstName = NamedType<std::string, struct FirstNameTag,Comparable,Printable,BinaryAddable>;
-using LastName = NamedType<std::string, struct LastNameTag,Comparable,Printable,BinaryAddable>;
-using FullName = NamedType<std::string, struct FullNameTag,Comparable,Printable,BinaryAddable>;
-
-FullName fullNameImpl(const FirstName& fn,const LastName& ln)
-{
-    return FullName(fn.get() + " " + ln.get());
-}
-static auto fullName = make_named_arg_function<FirstName,LastName>(fullNameImpl);
-
-TEST_CASE("NamedTypeNormal")
-{
-    FirstName name("Bob");
-    LastName surname("Hope");
-    auto fn = fullNameImpl(name,surname);
-
-    REQUIRE(fn == FullName("Bob Hope") );
+const auto errorHandler = [](const std::exception_ptr& e) {
+            try {
+                std::rethrow_exception(e);
+            } catch (const std::exception& ex) {
+                std::cerr << "Error: " << ex.what() << std::endl;
+            }
+        };
     
-}
+const auto completedHandler = []() {
+            std::cout << "Completed" << std::endl;
+        };
 
-TEST_CASE("NamedTypeAnyOrderCallable")
+TEST_CASE("RxCPP")
 {
-    FirstName name("Bob");
-    LastName surname("Hope");
-    auto fn = fullName(name,surname);
+    // Create an observable that emits values every 1 second
+    auto values = rxcpp::observable<>::interval(std::chrono::seconds(1))
+                      .map([](auto) { return "Tick"; });
 
-    REQUIRE(fn == FullName("Bob Hope") );   
+    // Subscribe to the observable
+    auto subscription = values.subscribe(
+        [](const std::string& value) {
+            std::cout << value << std::endl;
+        },
+        errorHandler,
+        completedHandler);
 }
 
-TEST_CASE("NamedTypeAnyOrderCallableReversedArguments")
+TEST_CASE("EmitedbyLambda")
 {
-    FirstName name("Bob");
-    LastName surname("Hope");
-    auto fn = fullName(surname,name);
 
-    REQUIRE(fn == FullName("Bob Hope") );   
+    auto numbers = rxcpp::observable<>::range(1,10);
+
+    auto timer = rxcpp::observable<>::interval(std::chrono::seconds(1));
+
+    auto values = timer.zip( numbers);
+
+    auto subscription = values.subscribe(
+        [](const auto& value) {
+            const auto& [idx,val] = value;
+            fmt::print("Idx {} : value {}\n",idx,val);
+        },
+        completedHandler);
 }
 
-
-// struct Foo
-// {
-//     Foo(){ spdlog::info(BOOST_CURRENT_FUNCTION);}
-//     void* operator new(std::size_t s)
-//     {
-//         spdlog::info(BOOST_CURRENT_FUNCTION);
-//         return ::operator new(s);
-//     }
-// };
-
-// TEST_CASE("OperatorNew")
-// {
-//     Foo * f = new Foo();
-//     delete f;
-// }
